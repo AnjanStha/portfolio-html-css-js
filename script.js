39a63441793131fe4c3d192d05e1d39496c2368d
Anjan Shrestha <anjanstha61@gmail.com>
2023-12-24 15:16:57 +0545

    try to add gallery section

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function toggleMenu(){
+// Function to toggle menu and hamburger icon
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('.typed-output', {
+    strings: ["Designer", "Developer", "Freelancer", "Photographer"],
+    typeSpeed: 100, // Adjust typing speed as needed
+    loop: true, // Set to true if you want the typing to loop
+    showCursor: true, // Enable default cursor display
+  });
+});
+
+
+
+
