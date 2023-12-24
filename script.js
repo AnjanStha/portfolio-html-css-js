4f752cbd3a9c79666166292b3840fa3f2c242f74
Anjan Shrestha <anjanstha61@gmail.com>
2023-12-24 22:50:41 +0545

    Portfolio website in final stage

--- script.js
+++ script.js
@@ -17,5 +17,44 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// for image slide
+
+
+
+let mainSliderSelector = '.main-slider',
+    thumbSliderSelector = '.thumb-slider';
+
+let mainSliderOptions = {
+    loop: true,
+    speed: 1500,
+    parallax: true,
+    loopAdditionalSlides: 5,
+    grabCursor: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+};
+
+let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+
+
+let thumbSliderOptions = {
+    loop: true,
+    speed: 1500,
+    loopAdditionalSlides: 5,
+    spaceBetween:5,
+    slidesPerView:5,
+    centeredSlides: true,
+    touchRatio:0.2,
+    slideToClickedSlide:true,
+    direction:'vertical' 
+};
+
+let thumbSlider = new Swiper(thumbSliderSelector, thumbSliderOptions);
+
+mainSlider.controller.control = thumbSlider;
+thumbSlider.controller.control = mainSlider;
 
 
